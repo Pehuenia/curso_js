04d6dfd46c001de5b370c8ab4631b7dd6cbfc0b3
Pehuenia <msaguero@gmail.com>
2023-06-04 04:59:17 -0300

    Se agregan funciones con Math

---
+++ curiosidadesMatematicas.js
@@ -0,0 +1,24 @@
+
+console.log(Math.round(2.2))
+
+//2.a
+let total = 10+3*8+5;
+
+console.log(total)
+
+//2.b
+console.log(total/3)
+
+//2.c
+let sumaCentavos = 1850+750 *2
+console.log(`Total a pagar por una tostadora más dos remeras: $${sumaCentavos/100}`)
+
+//2.d
+let totalConImpuestos = Math.round(sumaCentavos * 0.10) /100
+
+console.log(`Total a pagar con impuestos: $${totalConImpuestos}`)
+
+//2.e
+//let totalConImpuestos2 = 0
+
+console.log(`Monto en pesos: $${Math.round(sumaCentavos * 0.2)/100}`)
