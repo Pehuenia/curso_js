c75b4e4a3b9cdf7557ec65d6213eaa743626c1b4
Pehuenia <msaguero@gmail.com>
2023-06-06 17:23:02 -0300

    Se agregan Strings

---
+++ strings.js
@@ -0,0 +1,10 @@
+console.log('Items (2 + 5)')
+
+//
+let miString = 'Items (' + (1 + 1) + '): $' + Math.round(2095 + 799)/100 
+
+console.log(miString)
+
+console.log('I\'m a programming student.')
+
+console.log(`Hola`)
